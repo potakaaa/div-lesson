e14957eca66d2a2e9ad990a055e27692a19f6303
potakaaa <helbirog@gmail.com>
2025-01-19 20:50:27 +0800

    initialized repository

---
+++ src/App.tsx
@@ -0,0 +1,54 @@
+const App = () => {
+  return (
+    <div
+      id="web-container"
+      className="bg-zinc-900 w-full h-screen flex flex-col justify-center items-center"
+    >
+      <p className="text-white font-bold text-4xl">This is my App</p>
+      <div
+        id="parent-box-div"
+        className="border-2 border-white p-4 rounded-md mt-10 size-2/3 flex flex-col space-y-2"
+      >
+        {/* 1st DIV */}
+        <div id="group-indiv-div" className="flex flex-row size-full space-x-2">
+          <div
+            id="indiv-div"
+            className="border border-white p-4 rounded-md w-full text-white text-5xl font-bold flex justify-center items-center"
+          >
+            Hello Im Gerald
+          </div>
+        </div>
+
+        {/* 2nd DIV */}
+        <div id="group-indiv-div" className="flex flex-row size-full space-x-2">
+          <div
+            id="indiv-div"
+            className="border border-white p-4 rounded-md w-full"
+          ></div>
+          <div
+            id="indiv-div"
+            className="border border-white p-4 rounded-md w-full"
+          ></div>
+        </div>
+
+        {/* 3rd DIV */}
+        <div id="group-indiv-div" className="flex flex-row size-full space-x-2">
+          <div
+            id="indiv-div"
+            className="border border-white p-4 rounded-md w-full"
+          ></div>
+          <div
+            id="indiv-div"
+            className="border border-white p-4 rounded-md w-full"
+          ></div>
+          <div
+            id="indiv-div"
+            className="border border-white p-4 rounded-md w-full"
+          ></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
